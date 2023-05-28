bd9477dd78a094d35d8fb4604afe1ba67d56c41a
Juan Desimoni <juan.desimoni@codex.com.br>
2023-05-28 19:51:44 -0300

    Now handling includepdf and includegraphics

--- flatex.c
+++ flatex.c
@@ -7,17 +7,32 @@
 #define DEST_FILE "a.tex"
 #define MAX_DEPTH 7
 
-bool isinput(char *line) {
+char *isinput(char *line) {
 
-  char *input_ptr = strstr(line, "\\input{");
-  char *include_ptr = strstr(line, "\\include{");
-  char *comment_ptr = strchr(line, '%');
+  char *comment = strchr(line, '%');
+  char *input = strstr(line, "\\input{");
+  char *include = strstr(line, "\\include{");
 
-  if ((input_ptr && (!comment_ptr || (input_ptr < comment_ptr))) ||
-      (include_ptr && (!comment_ptr || (include_ptr < comment_ptr))))
-    return true;
-  else
-    return false;
+  if (input && (!comment || (input < comment)))
+    return input;
+  else if (include && (!comment || (include < comment)))
+    return include;
+
+  return NULL;
+}
+
+char *isincludepdf(char *line) {
+
+  char *comment = strchr(line, '%');
+  char *includepdf = strstr(line, "\\includepdf{");
+  char *includegraphics = strstr(line, "\\includegraphics[");
+
+  if (includepdf && (!comment || (includepdf < comment)))
+    return includepdf;
+  else if (includegraphics && (!comment || (includegraphics < comment)))
+    return includegraphics;
+
+  return NULL;
 }
 
 void flatit(char *source_path, FILE *dest, int *plevel) {
@@ -28,34 +43,48 @@ void flatit(char *source_path, FILE *dest, int *plevel) {
     return;
   }
 
-  printf(" %*s-> %s\n", 2*(*plevel), "", source_path);
+  printf(" %*s-> %s\n", 2 * (*plevel), "", source_path);
 
   ++*plevel;
 
+  size_t nread;
   size_t len = 1024;
   char *line = malloc(len);
-  while (getline(&line, &len, source) != -1) {
+  while ((nread = getline(&line, &len, source)) != -1) {
 
     if (errno != 0)
       break;
 
-    if (!isinput(line))
-      fputs(line, dest);
-    else {
+    if (!isinput(line)) {
 
-      if (*plevel+1 > MAX_DEPTH) {
+      char *s;
+      if ((s = isincludepdf(line)) != NULL) {
+
+        char *p = strchr(s, '{') + 1;
+        char *q = strrchr(p, '/') + 1;
+        char *end = line + nread;
+
+        for (unsigned short offset = q - p; p + offset < end; p++)
+          *(p) = *(p + offset);
+        *p = '\0';
+      }
+
+      fputs(line, dest);
+    } else {
+
+      if (*plevel + 1 > MAX_DEPTH) {
         fprintf(stderr, "Error: Recursion limit exceeded\n");
         errno = 1;
         break;
       }
 
-      char *start = strchr(line, '{') + 1;
-      char *end = strchr(start, '}');
+      char *p = line;
+      char *q = strchr(p, '{') + 1;
+      char *end = strchr(q, '}');
 
-      int i;
-      for (i = 0; start + i < end; i++)
-        line[i] = *(start + i);
-      line[i] = '\0';
+      for (unsigned short offset = q - p; p + offset < end; p++)
+          *(p) = *(p + offset);
+      *p = '\0';
 
       strcat(line, ".tex");
 
